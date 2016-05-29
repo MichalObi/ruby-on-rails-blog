e86dd5b06e6073c6ec7f8f16a382351902afa12e
Michal <ndwz123@gmail.com>
2016-05-29 18:58:23 +0200

    no error msg - check Migration:

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
+#ActiveRecord::Base - support for basic CRUD operations, database searching etc.
 class Article < ActiveRecord::Base
+	validates :title, 
+	presence: true,
+	length: { minimum: 5 }
 end
