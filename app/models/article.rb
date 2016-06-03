0522cf2ba55c0eaa8168424a50b23afeecf02970
Michal <ndwz123@gmail.com>
2016-06-03 21:43:10 +0200

    hotfix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 #ActiveRecord::Base - support for basic CRUD operations, database searching etc.
 class Article < ActiveRecord::Base
-	validates :title, 
-	presence: true,
-	length: { minimum: 5 }
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+
 end
