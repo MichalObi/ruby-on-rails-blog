17a3ca9d2c2c6b1a03a07eee5c84a744ff8a579c
Michal <ndwz123@gmail.com>
2016-07-03 12:49:32 +0200

    adding comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 #ActiveRecord::Base - support for basic CRUD operations, database searching etc.
 class Article < ActiveRecord::Base
+  has_many :comments
   validates :title, presence: true,
                     length: { minimum: 5 }
 
